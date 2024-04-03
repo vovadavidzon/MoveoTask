25a293b796e3c6e2709c611db0898d2b368fa5c3
vovadavidzon <vovadavidzon30@gmail.com>
2024-04-03 12:38:17 +0300

    added socket logic and connected code editor to socket

--- client/src/pages/CodePage.jsx
+++ client/src/pages/CodePage.jsx
@@ -6,36 +6,51 @@ import { io } from "socket.io-client";
 import Editor from "@monaco-editor/react";
 
 const CodePage = () => {
-  const [socket, setSocket] = useState(null);
   const location = useLocation();
   const data = location.state.code;
-  console.log(data);
+  const [socket, setSocket] = useState(null);
+  const [role, setRole] = useState(null);
+  const [updatedCode, setupdatedCode] = useState(data?.code);
 
   //intial socket
   useEffect(() => {
     const newSocket = io("http://localhost:3000"); //connect to socket server
     setSocket(newSocket);
+    if (data?._id !== "") {
+      newSocket.emit("join_code", data?._id);
+      newSocket.on("role", ({ role }) => {
+        setRole(role);
+        console.log(role);
+      });
+    }
+    newSocket.on("receive_update", (data) => {
+      setupdatedCode(data);
+    });
 
     return () => {
+      console.log("enterrrrrrr");
+
       newSocket.disconnect();
     };
   }, []);
 
+  const onEditorChange = (value, event) => {
+    socket.emit("update_code", { value, data });
+
+    console.log(value);
+  };
+
   return (
     <div>
-      {/* <Highlight language="javascript" className="custom-code-block">
-         
-        <input type="text" value={data.code} />
-      </Highlight> */}
       <Editor
         height="100vh"
         width="100%"
         theme="vs-dark"
         defaultLanguage="javascript"
-        defaultValue={data.code}
-        onChange={() => {
-          console.log("change");
-        }}
+        //defaultValue={updatedCode}
+        onChange={onEditorChange} // activate on every change
+        options={{ readOnly: role === "mentor" }}
+        value={updatedCode}
       />
     </div>
   );
--- socket/index.js
+++ socket/index.js
@@ -2,8 +2,37 @@ const { Server } = require("socket.io");
 
 const io = new Server({ cors: "http://localhost:5174" });
 
+let mentorExists = false;
+
 io.on("connection", (socket) => {
   console.log("new connection", socket.id);
+
+  let role = mentorExists ? "student" : "mentor";
+  console.log(io.sockets.sockets.size);
+
+  socket.on("join_code", (data) => {
+    socket.join(data);
+
+    socket.to(data).emit("role", { role });
+  });
+
+  socket.on("update_code", (obj) => {
+    console.log("its valuee", obj.data._id);
+    socket.to(obj.data._id).emit("receive_update", obj.value);
+  });
+
+  if (mentorExists === false) {
+    mentorExists = true;
+  }
+
+  socket.on("disconnect", () => {
+    if (role === "mentor") {
+      if (io.sockets.sockets.size === 0) {
+        mentorExists = false;
+      }
+    }
+    console.log("client disconnected");
+  });
 });
 
 io.listen(3000);
